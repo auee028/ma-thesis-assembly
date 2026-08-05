fixed ee rotation

robosuite/robosuite/custom_utils/assembly_controller.py
@@ -3,10 +3,18 @@ import time
 from robosuite.utils.transform_utils import mat2quat, convert_quat, quat2axisangle, axisangle2quat, quat_multiply, quat_distance
 from robosuite.custom_utils.voxposer_utils import get_clock_time, bcolors
 
+FMB_TASKS = ["assembly1", "assembly2", "assembly3"]
+PICK_ROTATION_EXCEPTIONS = {
+    "assembly1": ["obj3"],
+    "assembly2": ["obj2"],
+    "assembly3": ["obj4", "obj5"],
+}
+
 
 class WholeBodyIKController:
     def __init__(self, env, default_ee_pose=None):
         self.env = env
+        self.env_name = env.__class__.__name__.lower()
         
         self.last_ee_pos = self._get_gripper_pos()
         self.last_ee_quat = self._get_gripper_quat()
@@ -182,12 +190,17 @@ class WholeBodyIKController:
                 self.last_ee_quat = convert_quat(obs['robot0_eef_quat'], to='wxyz')
             self._action_terminated = False    # initialize the flag
     
+    def _no_rotation_for_pick(self, block_name):
+        exceptions = PICK_ROTATION_EXCEPTIONS.get(self.env_name.lower(), set())
+        return block_name in exceptions
+
+    
     def move_block(self, block_name, target_pos, target_ori=None):
         """
         Complete a block movement sequence using WholeBodyIK
         
         Args:
-            block_name: block name to move
+            block_name: body name of the block to move
             target_pos: [x,y,z]
             target_ori: [w,x,y,z]
         """
@@ -219,6 +232,8 @@ class WholeBodyIKController:
         block_quat_xyzw = convert_quat(block_quat, to='xyzw')
         quat_180_x = axisangle2quat([np.pi, 0, 0])   # 180° around X-axis (flip)
         quat_90_z = axisangle2quat([0, 0, -np.pi / 2])   # -90° around Z-axis
+        quat_p90_z = axisangle2quat([0, 0, np.pi / 2])   # 90° around Z-axis
+
         grasp_quat = convert_quat(
             quat_multiply(quat_180_x, quat_multiply(quat_90_z, block_quat_xyzw)),
             to='wxyz'
@@ -227,6 +242,20 @@ class WholeBodyIKController:
             quat_multiply(quat_90_z, convert_quat(np.array(self.default_ee_quat))),
             to='wxyz'
         )
+        if self.env_name.lower() in FMB_TASKS:
+            grasp_quat = self.default_ee_quat
+            block_name_stem = block_name.replace("_main", "")
+            if self._no_rotation_for_pick(block_name_stem):
+                grasp_quat = convert_quat(
+                quat_multiply(quat_90_z, convert_quat(np.array(self.default_ee_quat))),
+                to='wxyz'
+            )
+
+            place_quat = convert_quat(
+                quat_multiply(quat_p90_z, convert_quat(self._get_eef_quat())),
+                to='wxyz'
+            )
+
         self.move_to_pose(block_pos, quaternion=grasp_quat, phase='reach', ori_thresh=1.6, num_steps=35)
         
         print(f"{bcolors.OKGREEN}[assembly_controller.py | {get_clock_time()}] Closing gripper{bcolors.RESET}")

robosuite/robosuite/demos/demo_passive_viewer_fmb.py
@@ -11,6 +11,9 @@ from robosuite.controllers import load_composite_controller_config
 from robosuite.utils.camera_utils import get_camera_transform_matrix, get_real_depth_map
 from robosuite.utils.transform_utils import quat2axisangle, mat2quat
 
+from robosuite.custom_utils.assembly_llm import AssemblyLLM
+from robosuite.custom_utils.assembly_spatial_graph import AssemblySpatialGraph
+from robosuite.custom_utils.assembly_planner import AssemblyPlanner
 from robosuite.custom_utils.assembly_utils import *
 
 
@@ -45,43 +48,209 @@ env = suite.make(
 # Define camera views to capture
 camera_names = env.sim.model.camera_names
 
+# Adjust the camera pose so that the 'agentview' can capture all blocks on the table
+cam_id = env.sim.model.camera_name2id("agentview")
+env.sim.model.cam_pos[cam_id] = np.array([0.6, 0.0, 1.6])
+
 # Access the geometries' names and ids
 geom_names = env.sim.model.geom_names
+# print(geom_names)
+
+# print("Bodies:")
+# print(env.sim.model.body_names)
+
+# print("Geoms:")
+# print(env.sim.model.geom_names)
+
+# Create a directory to save images
+os.makedirs(configs["task"]["target_rgb_dir"], exist_ok=True)
+
+# save_dir = os.path.join(configs["save_root"], f'captured_images_{datetime.today().strftime("%Y%m%d-%H%M%S")}')
+save_dir = os.path.join(configs["save_root"], f'captured_images_{datetime.today().strftime("%Y%m%d-%H%M%S")}_thesis_{configs["exp_id"]}_{env_config["env_name"]}')
+if not configs["llm"]["model_config"]["top_p"]:    # if setting of top_p is null
+    save_dir += "_topp1"    # top_p=1.0 as default
+os.makedirs(save_dir, exist_ok=True)
+
+# Create a directory to save input rgb and visualization
+os.mkdir(os.path.join(save_dir, "assembly_vis"))
+os.mkdir(os.path.join(save_dir, "assembly_input_img"))
+
+# Save the current files
+os.system(f"cp {config_path} {os.path.join(save_dir, os.path.basename(config_path))}")
+
+system_prompt = configs["relation"]["prim_prompt_files"]["system_prompt"]
+os.system(f"cp {system_prompt} {os.path.join(save_dir, os.path.basename(system_prompt))}")
+
+examples_prompt = configs["relation"]["prim_prompt_files"]["examples_prompt"]
+os.system(f"cp {examples_prompt} {os.path.join(save_dir, os.path.basename(examples_prompt))}")
 
 # Reset the environment
-# # env.reset()
-# obs = env.reset()
+env.reset()
 
-# try:
-#     while True:
-#         action = np.zeros(env.action_dim)
-#         obs, reward, done, info = env.step(action)
-#         time.sleep(1 / 60)
+"""
+# Capture and save images from multiple views before assembling
+obs = env._get_observations()
+for cam_name in camera_names:
+    # Ensure camera's RGB channel exists in observation and then get and save the RGB image
+    rgb_cam_name = cam_name + "_image"
+    if rgb_cam_name in obs.keys():
+        rgb_img = obs[rgb_cam_name]  # Get image from the observation
+        save_path = os.path.join(save_dir, f"before_assembling_{cam_name}_frame.png")
+        save_cam_image(rgb_img, save_path)
+        
+        # Save for target rgb image fed together with target query
+        if configs["task"]["target_cam"] == cam_name:
+            save_path = configs["task"]["target_rgb_path"]
+            save_cam_image(rgb_img, save_path)
+"""
 
-# except KeyboardInterrupt:
-#     print("\nExiting...")
+# Set up modules of LLM and Graph
+llm = AssemblyLLM(configs)
+g = AssemblySpatialGraph()
 
-# finally:
-#     env.close()
+# Iterate
+for i in range(configs["num_iter"]):
+    # Get information of interested objects
+    block_geom_names = tuple(
+        geom_name
+        for geom_name in geom_names 
+        # if any(shape in geom_name for shape in shapes) and '_vis' in geom_name
+        if geom_name.startswith("obj") and geom_name.endswith("_visual")
+    )
+    block_geom_ids = tuple(
+        env.sim.model.geom_name2id(geom_name) for geom_name in block_geom_names
+    )
+    block_colors = tuple(
+        get_color_name(env.sim.model.geom_rgba[geom_id], RGBA2COLORNAME)
+        for geom_id in block_geom_ids
+    )
+    block_info = {
+        # geom_name.replace("visual", "vis"): {"geom_id": geom_id, "color": color}
+        geom_name.replace("_visual", ""): {"geom_id": geom_id, "color": color}
+        for geom_name, geom_id, color in zip(block_geom_names, block_geom_ids, block_colors)
+    }
+    print("Block info: ", block_info)
 
-obs = env.reset()
-
-while True:
-    site_name = "gripper0_right_grip_site"
-    site_id = env.sim.model.site_name2id(site_name)
-
-    pos = env.sim.data.site_xpos[site_id].copy()
-    mat = env.sim.data.site_xmat[site_id].reshape(3, 3).copy()
-
-    quat = mat2quat(mat)
-    axis_angle = quat2axisangle(quat)
-
-    action = np.concatenate([
-        pos,
-        axis_angle,
-        [0.0],    # gripper
-    ])
-
-    obs, reward, done, info = env.step(action)
+    # Map colors to available geom_names
+    color_to_geom_names = defaultdict(list)
+    for geom_name, info in block_info.items():
+        color = info["color"]
+        if color:
+            color_to_geom_names[color].append(geom_name)
+        
+    # Save image of the environment in each episode
+    obs = env._get_observations()
+    rgb_cam_name = configs["task"]["target_cam"] + "_image"
+    rgb_img = obs[rgb_cam_name]  # Get image from the observation
+    save_path = configs["task"]["target_rgb_path"]
+    save_cam_image(rgb_img, save_path)
+    save_path = os.path.join(save_dir, "assembly_input_img", f"assembly_rgb_{i}.png")
+    save_cam_image(rgb_img, save_path)
     
+    # Get llm response
+    # detected_blocks, instructions = llm(log_res_path=os.path.join(save_dir, "log_llm_res.txt"))    # returns detected_blocks and assembly_structure
+    # content = llm(log_res_path=os.path.join(save_dir, "log_llm_res.txt"))    # returns 'content' of response from LLM
+    content = {
+        "detected_blocks": [
+            {
+            "block_id": "block0",
+            "color": "purple"
+            },
+            {
+            "block_id": "block1",
+            "color": "brown"
+            },
+            {
+            "block_id": "block2",
+            "color": "green"
+            },
+            {
+            "block_id": "block3",
+            "color": "red"
+            },
+            {
+            "block_id": "block4",
+            "color": "blue"
+            }
+        ],
+        "assembly_structure": [
+            "block0 is the purple fixture.",
+            "block3 is inserted into block0 at the leftmost position.",
+            "block1 is inserted into block0 at the left position.",
+            "block2 is inserted into block0 at the right position.",
+            "block4 is inserted into block0 at the rightmost position."
+        ]
+    }
+
+    detected_blocks = content["detected_blocks"]
+    
+    # Match detection results and real block names in robosuite env
+    block_matches = match_block_names(detected_blocks, color_to_geom_names)
+    print("Block matches: ", block_matches)
+        
+    if "structure_relations" in content.keys():
+        assembly_structure = content["structure_relations"]
+        assembly_order, new_block_positions = compute_positions_from_distances(assembly_structure)
+        print("* * *")
+        # print("Assembly order: ", assembly_order)
+        print("New block positions: ", new_block_positions)
+        
+    else:    # "assembly_structure" in content.keys()
+        instructions = content["assembly_structure"]
+        print("* * *")
+        print("Instructions: ", instructions)
+
+        # Get assembly order
+        spatial_graph, assembly_order = g(instructions)
+        print("Assembly order: ", assembly_order)
+
+        # Calculate 3d coordinates of individual blocks and log them
+        planner = AssemblyPlanner(env, block_matches)
+        new_block_positions = planner._compute_block_positions(spatial_graph, assembly_order)
+        print("New block positions: ", new_block_positions)
+    
+    # Get color information
+    block_colors = {}
+    for det in detected_blocks:
+        if "block_id" in det.keys():
+            det_id = det["block_id"]
+        else:
+            det_id = det["id"]
+        det_color = det["color"]
+        block_colors[det_id] = det_color
+        
+    # Save visualized assembly result
+    save_file_path = os.path.join(save_dir, "assembly_vis", f"assembly_res_{i}.png")
+    visualize_structure(
+        assembly_order,
+        new_block_positions,
+        block_matches,
+        block_colors,
+        configs["task"]["target_query_structure"],
+        save_file_path)
+    print(save_file_path)
+    print("* * *")
+    
+    # Reset the environment
+    env.reset()
+
+# Plan and execute
+planner = AssemblyPlanner(env, block_matches)
+planner(spatial_graph, assembly_order)
+
+# Adjust pose of 'sideview' camera
+cam_id = env.sim.model.camera_name2id("sideview")
+env.sim.model.cam_pos[cam_id] = np.array([0.0, 0.9, 1.2])
+
+# Capture and save images from multiple views after assembling
+obs = env._get_observations()
+for cam_name in camera_names:
+    # Ensure camera's RGB channel exists in observation and then get and save the RGB image
+    rgb_cam_name = cam_name + "_image"
+    if rgb_cam_name in obs.keys():
+        rgb_img = obs[rgb_cam_name]  # Get image from the observation
+        save_path = os.path.join(save_dir, f"after_assembling_{cam_name}_frame.png")
+        save_cam_image(rgb_img, save_path)
+
 env.close()
+

robosuite/robosuite/environments/manipulation/fmb_assembly3.py
@@ -449,9 +449,11 @@ class Assembly3(ManipulationEnv):
             self.obj5: [0.12, 0.3, self.table_offset[2] + 0.045], # [0.1, 0.15, self.table_offset[2] -0.005] (before recentering .obj file)
         }
 
-        quat = np.array([1, 0, 0, 0])   # no rotation
-
-        for obj, pos in placements.items():
+        for i, (obj, pos) in enumerate(placements.items()):
+            quat = np.array([1, 0, 0, 0])   # no rotation
+            if i == 0:  # fixture
+                quat = np.array([np.sqrt(2)/2, 0.0, 0.0, np.sqrt(2)/2])   # 90 degree rotation along z axis
+                
             self.sim.data.set_joint_qpos(
                 obj.joints[0],
                 np.concatenate([np.array(pos), quat])